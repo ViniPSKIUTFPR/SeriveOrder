05a898151a0673a80c1dd50204ee32663c4d1324
Vinicius <vinix464@gmail.com>
2025-05-01 20:46:31 -0300

    first commit

---
+++ Order-Serivce-management/src/main/java/utfpr/arq/Order/Serivce/management/OrderSerivceManagementApplication.java
@@ -0,0 +1,13 @@
+package utfpr.arq.Order.Serivce.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSerivceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSerivceManagementApplication.class, args);
+	}
+
+}
